73da3ea321c1f6548fdba9ea4f21012345a32af7
YLiao12 <seu_lyq@outlook.com>
2021-12-26 18:03:40 +0800

    ready delay cancel

--- app/src/main/java/hk/edu/cuhk/ie/group23/GameActivity.java
+++ app/src/main/java/hk/edu/cuhk/ie/group23/GameActivity.java
@@ -676,12 +676,6 @@ public class GameActivity extends AppCompatActivity {
                 body.close();
             }
         });
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     public void win(View view) {
